82a0696193c7bca7ad3b82efbd323377673c6270
Kalle Soranko <35808136+kallesoranko@users.noreply.github.com>
2019-05-03 05:41:25 +0300

    use StringUtils.isEmpty instead of checking if null (#5)

---
+++ src/test/java/io/verifa/cantata/CantataRunTestBuilderTest.java
@@ -0,0 +1,22 @@
+package io.verifa.cantata;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Label;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class CantataRunTestBuilderTest {
+
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+    final String name = "Bobby";
+
+    
+
+}
